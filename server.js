791e05e96b9a93b899d58db74d225730e1f58ee2
FrostNIOST <santiago.f.martinez.p@gmail.com>
2024-05-16 09:56:23 -0500

    cors corregido

--- server.js
+++ server.js
@@ -2,17 +2,20 @@
  * @name server.js
  */
 
+
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 //setqings
+
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 //middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 //routes
 app.use(require('./scr/routers/routes_cliente'));
